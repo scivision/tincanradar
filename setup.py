a79fd67308a7d3dd5fd09d1303af288aa93797a4
scivision <scivision@users.noreply.github.com>
2017-02-12 04:42:10 -0500

    pythonic

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 from setuptools import setup #enables develop
-import subprocess
 
-try:
-    subprocess.run(['conda','install','--yes','--file','requirements.txt']) #don't use os.environ
-except Exception as e:
-    print('you will need to install packages in requirements.txt  {}'.format(e))
+req =['nose','numpy','matplotlib','seaborn']
 
-with open('README.rst','r') as f:
-	  long_description = f.read()
+import pip
+pip.main(['install','numpy'])
 
 #%%
 from numpy.distutils.core import setup,Extension
@@ -17,9 +13,9 @@ from numpy.distutils.core import setup,Extension
 setup(name='tincanradar',
       version='0.1',
 	  description='Model and Build a $35 radar from coffee cans and MMICs',
-	  long_description=long_description,
-	  author='Michael Hirsch',
+	  author='Michael Hirsch, Ph.D.',
 	  url='https://github.com/scivision/tincanradar',
+      install_requires=req,
       packages=['tincanradar'],
       ext_modules=[Extension(name='pychirp',
                     sources=['comm.f90','fwdmodel.f90'],
