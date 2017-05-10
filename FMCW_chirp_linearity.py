9c9a8e3334b0c15ed1734a7b993b2487efe61b32
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-10 17:03:41 -0400

    doc

---
+++ FMCW_chirp_linearity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+Non-linear FMCW chirp arises from numerous issues. For VCOs and YIGs, the
+finite analog hardware bandwidth of the modulation (chirp) input is a key limiting factor.
+This is a simple model of such behavior.
+The big impact of FMCW non-linear chirp is on radar accuracy.
+"""
