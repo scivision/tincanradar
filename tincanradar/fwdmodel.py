02d89b6e551c49450e9cae3a6011d07ed627ef4b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-09-15 17:28:02 -0400

    doc

--- tincanradar/fwdmodel.py
+++ tincanradar/fwdmodel.py
@@ -1,17 +1,15 @@
-from __future__ import division
 from time import time
-from sys import stderr
-from numpy import log10,pi,exp,asarray,arange
+import warnings
+from numpy import log10,pi,exp,asarray,arange,ndarray
 from scipy.signal import firwin,lfilter,resample
 try:
     import pychirp as fwd
 except ImportError:
     fwd = None
-    print('falling back to slow non-Fortran method',file=stderr)
-
+    warnings.warn('falling back to slow non-Fortran method')
 #
 c = 299792458.
-Atarg = [0.2]
+Atarg = [0.2] # target amplitude, arbitrary (and often very small!)
 
 def friis(range_m,freq,exp=2):
     return 10*log10((4*pi * freq / c)**2 * range_m**exp)
@@ -36,9 +34,9 @@ def fmcwtransceive(bm,tm,range_m,adcbw,adcfs,tfs,nlfm=0.):
     print(f'{time()-tic:.3f} sec to compute time-domain chirp')
 #%% mixer lpf
     tic=time()
-    h = firwin( numtaps=100, cutoff=adcbw, nyq=tfs/2.)
-    y = lfilter( h, 1., y)
-    print('{:.3f} sec to anti-alias filter'.format(time()-tic))
+    h = firwin(numtaps=100, cutoff=adcbw, nyq=tfs/2.)
+    y = lfilter(h, 1., y)
+    print(f'{time()-tic:.3f} sec to anti-alias filter')
 
     Y,t = resample(y,int(y.size * adcfs / tfs),t)
     return Y,t
@@ -51,12 +49,25 @@ def FMCWnoisepower(NF,adcbw):
     #RXbw = 2/tm # [Hz]
 
     return -174.4 + NF + 10*log10(adcbw) #[dBm]
-#%% FMCW
-def chirprx(bm,tm,t,range_m, Atarg, nlfm=0.):
+# %% FMCW
+def chirprx(bm:float, tm:float, t, range_m, Atarg, nlfm:float=0.):
+    """
+    inputs
+    -------
+    bm: scalar: bandwidth of chirp [Hz]
+    tm: scalar: time length of chirp [sec]
+    t: vector of time samples [sec]
+    range_m: scalar or vector: target range(s) [m]
+    A_targ: scalar or vector:  target amplitude(s) [V]
+    nlfm: scalar: chirp non-linearity factor (0. for perfectly linear)
 
+    outputs
+    -------
+    lo: output of transmitter VCO, YIG, or DAC that is amplified and sent out transmit antenna
+    xtargs: ideal reflected signal from targets with round-trip delay.
+    """
     lo = chirptx(bm,tm,t,nlfm)
-#%% targets
-
+# %% targets
     range_m = asarray(range_m)
 
     toffs = 2 * range_m/c
@@ -64,11 +75,11 @@ def chirprx(bm,tm,t,range_m, Atarg, nlfm=0.):
     if t.size*range_m.size*8 > 8e9:
         raise MemoryError(f'too much RAM used {t.size*range_m.size*8:.1e} B')
 
-    xtargs = Atarg * chirptx(bm,tm,t+toffs, nlfm)
+    xtargs = Atarg * chirptx(bm, tm, t+toffs, nlfm)
 
     return xtargs, lo
 
-def chirptx(bm,tm,t,nlfm):
+def chirptx(bm:float, tm:float, t:ndarray, nlfm:float):
 
     B1 = bm / tm
     B2 = bm / tm**2
